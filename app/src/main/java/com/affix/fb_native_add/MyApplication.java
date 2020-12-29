b2c4c0615d60edbb8ea24723d82d0528fd813812
saliyaambegoda <ayilas419@gmail.com>
2020-12-29 21:28:33 +0530

    Facebook Native adds

---
+++ app/src/main/java/com/affix/fb_native_add/MyApplication.java
@@ -0,0 +1,13 @@
+package com.affix.fb_native_add;
+
+import android.app.Application;
+
+import com.facebook.ads.AudienceNetworkAds;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AudienceNetworkAds.initialize(this);
+    }
+}
